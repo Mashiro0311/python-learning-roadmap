17e4e721251cf7ed1b34ccf42acf9f9bb1cfb1a9
Mashiro0311 <1014376920@qq.com>
2026-01-07 11:38:37 +0800

    小项目第二天： 爬取结构化数据并保存为CSV

--- 05_projects/quotes_crawler_and_analysis/crawler.py
+++ 05_projects/quotes_crawler_and_analysis/crawler.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,9 +18,22 @@ for i in range(1, 4):
         continue
 
     soup = BeautifulSoup(response.text, "html.parser")
-    quotes = soup.find_all("span", class_="text")
-    for q in quotes:
-        all_quotes.append(q.text)
+    blocks = soup.find_all("div", class_="quote")
+    for block in blocks:
+        quote = block.find("span", class_="text").text
+        author = block.find("small", class_="author").text
+        all_quotes.append({
+            "quote": quote,
+            "author": author
+        })
+    # print(all_quotes)
     print(f"第 {i} 页抓取完成，当前共 {len(all_quotes)} 条")
+with open("quotes.csv", "w", encoding="utf-8") as f:
+    writer = csv.DictWriter(f, fieldnames=["quote", "author"])
+    writer.writeheader()
+    writer.writerows(all_quotes)
+    f.close()
+
+print("数据已成功保存为 quotes.csv")
 print("==========")
 print("爬取结束，总条数：", len(all_quotes))
